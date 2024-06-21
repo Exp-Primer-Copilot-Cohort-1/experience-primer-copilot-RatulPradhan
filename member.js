16c488387faa70b91fdd793bc824f0d747629fe9
Ratul Pradhan <109481340+RatulPradhan@users.noreply.github.com>
2024-06-21 17:51:51 +0000

    adding member.js

---
+++ member.js
@@ -0,0 +1,26 @@
+function skillsMember() {
+    return {
+        "skills": [
+            {
+                "name": "JavaScript",
+                "level": "Intermediate"
+            },
+            {
+                "name": "React",
+                "level": "Intermediate"
+            },
+            {
+                "name": "Vue",
+                "level": "Intermediate"
+            },
+            {
+                "name": "Angular",
+                "level": "Intermediate"
+            },
+            {
+                "name": "Node.js",
+                "level": "Intermediate"
+            }
+        ]
+    }
+}
